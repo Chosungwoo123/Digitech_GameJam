c2d3481ff499763638bba8860e757697ad4a481c
Chosungwoo123 <ejyoo12345@naver.com>
2024-01-13 20:00:20 +0900

    Make a Player Jump.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,18 +8,37 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce = 16;
 
+    #region Ground Check
+
+    [Space(10)]
+    [Header("Ground Check")]
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private int jumpCount;
+
+    #endregion
+
+    private int jumpCounter;
+
     private float movementInputDirection;
+
+    private bool isGrounded;
+    private bool canJump;
 
     private Rigidbody2D rigid;
 
     private void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        jumpCounter = jumpCount;
     }
 
     private void Update()
     {
         CheckInput();
+        CheckSurroundings();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate()
@@ -37,6 +56,28 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void CheckIfCanJump()
+    {
+        if (jumpCounter > 0)
+        {
+            canJump = true;
+        }
+        else if (isGrounded)
+        {
+            canJump = true;
+            jumpCounter = jumpCount;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+
     private void MoveUpdate()
     {
         rigid.velocity = new Vector2(moveSpeed * movementInputDirection, rigid.velocity.y);
@@ -44,6 +85,17 @@ public class PlayerMovement : MonoBehaviour
 
     private void Jump()
     {
-        rigid.velocity = new Vector2(rigid.velocity.x, jumpForce);
+        if (canJump)
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, jumpForce);
+            jumpCounter--;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
