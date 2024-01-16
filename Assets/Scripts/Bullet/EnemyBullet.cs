60cd12a90ca04efd6e2b949ad66c5a89e0db74ca
Chosungwoo123 <ejyoo12345@naver.com>
2024-01-16 14:49:34 +0900

    Make a Test Boss Pattern.

--- Assets/Scripts/Bullet/EnemyBullet.cs
+++ Assets/Scripts/Bullet/EnemyBullet.cs
@@ -6,11 +6,28 @@ public class EnemyBullet : MonoBehaviour
 {
     [SerializeField] private float damage;
     public GameObject effect;
+    private bool isPenetrate;
+    private int penatrateCount;
 
-    public void Init(float speed, float damage)
+    public void Init(float speed, float damage, bool penetrate = false, float lifeTime = 1f, int penatrateCount = 1)
     {
         this.damage = damage;
+        isPenetrate = penetrate;
+        this.penatrateCount = penatrateCount;
         gameObject.GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+
+        if (isPenetrate)
+        {
+            StartCoroutine(LifeRoutine(lifeTime));
+        }
+    }
+    
+    private IEnumerator LifeRoutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        Instantiate(effect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,8 +41,12 @@ public class EnemyBullet : MonoBehaviour
 
         if (collision.CompareTag("Ground"))
         {
-            Instantiate(effect, transform.position, Quaternion.identity);
-            Destroy(gameObject);
+            penatrateCount--;
+            if (penatrateCount <= 0)
+            {
+                Instantiate(effect, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
         }
     }
 }
--- Assets/Scripts/Enemy/Boss.cs
+++ Assets/Scripts/Enemy/Boss.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float maxHealth;
+    public EnemyBullet bulletPrefab;
+
+    private float curHealth;
+
+    private void Start()
     {
-        
+        curHealth = maxHealth;
+
+        StartCoroutine(Pattern01());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Pattern01()
     {
-        
+        int shootCount = 100;
+        int bulletCount = 4;
+
+        WaitForSeconds interval = new WaitForSeconds(0.1f);
+
+        float offset = 0;
+
+        for (int i = 0; i < shootCount; i++)
+        {
+            for (int j = 0; j < 360; j += 360 / bulletCount)
+            {
+                Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0, 0, j + offset)).Init(10, 10, true, 5, 4);
+                Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0, 0, -(j + offset))).Init(10, 10, true, 5, 4);
+            }
+
+            yield return interval;
+
+            offset += 5f;
+        }
+    }
+
+    public void OnDamage(float damage)
+    {
+        curHealth -= damage;
     }
 }
